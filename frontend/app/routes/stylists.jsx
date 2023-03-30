eeb24f58041005d16cc2d794abfafd990d0dcd15
Fdlaurentis <101360874+Fdlaurentis@users.noreply.github.com>
2023-03-30 08:31:42 -0500

    work on the screens of services and stylists (#36)
    work on the screens of services and stylists
    
    Co-authored-by: Alberto Gómez Juan <agomezjuan@hotmail.com>

---
+++ frontend/app/routes/stylists.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Stylists } from '../pages/Stylists'
+
+
+export default function stylists() {
+    return (
+        <main>
+            <Stylists />
+        </main>
+    )
+
+}
