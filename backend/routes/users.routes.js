b18ca4bb06b09c5369614f018a59c8a81dee4c73
julandrod <julian.andres.rodriguez@gmail.com>
2023-04-04 11:14:55 -0500

    (backend) <add> rutas para obtener y eliminar usuario por id

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const { checkSchema } = require('express-validator');
-const { authenticateUser } = require('../middlewares/auth.middleware');
-const { getAllUsers, showMe } = require('../controllers/users.controllers');
+const express = require("express");
+const { checkSchema } = require("express-validator");
+const { authenticateUser } = require("../middlewares/auth.middleware");
+const {
+  getAllUsers,
+  showMe,
+  getUserById,
+  updateUserById,
+  deleteUser,
+} = require("../controllers/users.controllers");
 
 const router = express.Router();
 
-router.get('/showMe', authenticateUser, showMe);
-router.get("/:roleUser", authenticateUser, getAllUsers)
+router.get("/showMe", authenticateUser, showMe);
+router.get("/:id", authenticateUser, getUserById);
+router.patch("/:id", authenticateUser, updateUserById);
+
+// Ruta de actualizar usuario por id
+router.delete("/:id", authenticateUser, deleteUser);
+
+router.get("/:roleUser", authenticateUser, getAllUsers);
 
 module.exports = router;
-
