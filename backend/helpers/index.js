78d291227d9b114264cf88dcb2ff9837f0fc1a0b
julandrod <julian.andres.rodriguez@gmail.com>
2023-04-04 15:29:54 -0500

    (backend) <add> funcion para comprobar que sea el usuario activo quien puede modificar su propia informacion en los endpoints

--- backend/helpers/index.js
+++ backend/helpers/index.js
@@ -1,11 +1,12 @@
+const checkPermissions = require("./checkPermissions");
 const endPointResponse = require("./endPointReponse");
 const CustomError = require("./errorResponse");
 const { createJwt, isTokenValid } = require("./handleJwt");
 const { encryptPassword, comparePassword } = require("./handlePassword");
 const tryCatchWrapper = require("./tryCatchWrapper");
 
-
 module.exports = {
+  checkPermissions,
   endPointResponse,
   CustomError,
   createJwt,
