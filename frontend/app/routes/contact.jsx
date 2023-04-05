ef616475a665e4c9413723d7938c1c5b0445b7e2
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-04-05 12:11:21 -0500

    add contact in routes and navbar

---
+++ frontend/app/routes/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Contact } from '../pages/Contact'
+
+
+export default function services() {
+    return (
+        <main>
+            <Contact />
+        </main>
+    )
+
+}
