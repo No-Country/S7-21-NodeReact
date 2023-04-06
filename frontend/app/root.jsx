a5bff0031556508a30322931268c182e3fd0d2be
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-04-06 12:24:52 -0500

    add implement login

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -6,9 +6,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { Provider } from "react-redux";
 import Navbar from "~/components/Navbar";
 
 import styles from "~/styles/main.css"
+import { store } from "./store/store";
 
 export const meta = () => ({
   charset: "utf-8",
@@ -24,11 +26,13 @@ export default function App() {
         <Links />
       </head>
       <body>
+      <Provider store={store}>
         <Navbar />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        </Provider>
       </body>
     </html>
   );
--- frontend/app/store/auth/authSlice.js
+++ frontend/app/store/auth/authSlice.js
@@ -1,33 +1,60 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-        status: 'checking', //'authenticated', 'not-authenticated'
-        user: {},
-        errorMessage: undefined,
+  name: "auth",
+  initialState: {
+    status: "checking", //'authenticated', 'not-authenticated'
+    user: {},
+    errorMessage: undefined,
+  },
+  reducers: {
+    onChecking: (state) => {
+      state.status = "checking";
+      state.user = {};
+      state.errorMessage = undefined;
     },
-    reducers: {
-        onChecking: (state) => {
-            state.status = 'checking'
-            state.user = {}
-            state.errorMessage = undefined
-        },
-        onLogin: (state, { payload }) => {
-            state.status = 'authenticated'
-            state.user = payload
-            state.errorMessage = undefined
-        },
-        onLogout: (state, { payload }) => {
-            state.status = 'not-authenticated'
-            state.user = {}
-            state.errorMessage = payload
-        },
-        clearErrorMessage: (state) => {
-            state.errorMessage = undefined
-        }
-    }
+    onLogin: (state, { payload }) => {
+      state.status = "authenticated";
+      state.user = payload;
+      state.errorMessage = undefined;
+    },
+    onLoginError: (state, { error }) => {
+      state.status = "not-authenticated";
+      state.user = {};
+      state.errorMessage = error;
+    },
+    onLogout: (state, { payload }) => {
+      state.status = "not-authenticated";
+      state.user = {};
+      state.errorMessage = payload;
+    },
+    clearErrorMessage: (state) => {
+      state.errorMessage = undefined;
+    },
+  },
 });
+export const loginuser = (payload) => {
+  return async (dispatch) => {
+    console.log(payload, "datos")
+    try {
+      dispatch({ type: onChecking });
+      const {data}= await axios.post("http://localhost:8080/api/v1/auth/login", payload);
+      console.log(data, "json")
+      if (data) {
+        dispatch({ type: onLogin, payload: data.body.user });
+        return data;
+      }
+    } catch (error) {
+      dispatch({ type: onLoginError, payload: error });
+    }
+  };
+};
 
-
-export const { onChecking, onLogin, onLogout, clearErrorMessage } = authSlice.actions;
+export const {
+  onChecking,
+  onLogin,
+  onLogout,
+  clearErrorMessage,
+  onLoginError,
+} = authSlice.actions;
+export default authSlice.reducer;
