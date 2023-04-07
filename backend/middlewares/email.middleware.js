bc5400bf9de78cc1b515a81e6e63200ca7f00c22
julandrod <julian.andres.rodriguez@gmail.com>
2023-04-07 09:29:43 -0500

    (backend) <refactor> se agregan los parametros verification token y origin

--- backend/middlewares/email.middleware.js
+++ backend/middlewares/email.middleware.js
@@ -1,25 +1,37 @@
-const transporter = require('../helpers/notifactions/gmail/config.js')
-const gmailOptions = require('../helpers/notifactions/gmail/emailSender.js')
-const emailNewUser = require('../helpers/notifactions/templates/newUserEmail.js')
+const transporter = require("../helpers/notifactions/gmail/config.js");
+const gmailOptions = require("../helpers/notifactions/gmail/emailSender.js");
+const emailNewUser = require("../helpers/notifactions/templates/newUserEmail.js");
 
 /**
  * Middleware para el envío de mails a usuarios nuevos.
  * @param {String} email email de usario nuevo
  * @param {String} userName nombre del usuario nuevo
  * @param {String} lastName apellido del usuario nuevo
- * 
+ *
  * @example
  * await sendNewUser(email, firstName, lastName)
  */
 
-async function sendNewUser(email, userName, lastName) {
-    try {
-        const htmlTemplate = emailNewUser(userName, lastName)
-        const emailSubject = 'Cuenta Nueva'
-        await transporter.sendMail(gmailOptions(email, emailSubject, htmlTemplate))
-    } catch (err) {
-        console.log('Error al leer el archivo HTML: ', err)
-    }
+async function sendNewUser(
+  email,
+  userName,
+  lastName,
+  verificationToken,
+  origin
+) {
+  try {
+    const htmlTemplate = emailNewUser(
+      userName,
+      lastName,
+      email,
+      verificationToken,
+      origin
+    );
+    const emailSubject = "Cuenta Nueva";
+    await transporter.sendMail(gmailOptions(email, emailSubject, htmlTemplate));
+  } catch (err) {
+    console.log("Error al leer el archivo HTML: ", err);
+  }
 }
 
-module.exports = sendNewUser
+module.exports = sendNewUser;
