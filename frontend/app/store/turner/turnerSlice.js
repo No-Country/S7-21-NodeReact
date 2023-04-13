e2ceac791000753666c214c54624e607706bdb5a
fredyvaron <fvaron@itfip.edu.co>
2023-04-13 16:35:17 -0500

    add redux get turner

--- frontend/app/store/turner/turnerSlice.js
+++ frontend/app/store/turner/turnerSlice.js
@@ -5,8 +5,11 @@ export const turnerSlice = createSlice({
   name: "turner",
   initialState: {
     loading: true,
+    loadingTurner: true,
     turner: {},
+    turners: [],
     errorMessage: undefined,
+    errorMessageTurner: undefined,
   },
   reducers: {
     onLoading: (state) => {
@@ -24,6 +27,21 @@ export const turnerSlice = createSlice({
       state.turner = {};
       state.errorMessage = error;
     },
+    onLoadingTurner:(state, {payload}) => {
+      state.loadingTurner = true;
+      state.turners = []
+      state.errorMessage = undefined
+    },
+    onTurnerHistory: (state, { payload }) => {
+      state.loadingTurner = false;
+      state.turners = payload
+      state.errorMessage = undefined
+    },
+    onMessageTurnersError: (state, { error }) => {
+      state.loadingTurner = false;
+      state.turners = []
+      state.errorMessage = error
+    }
   },
 });
 export const getHeadersWithAuth = (token) => {
@@ -34,7 +52,7 @@ export const getHeadersWithAuth = (token) => {
 export const createturne = (payload, barber) => {
   return async (dispatch) => {
     const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNmNWUyMWM2LThhODUtNDEwYy04OWVjLWQyNjllMjIwZDI4YSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4MTM4ODM0NiwiZXhwIjoxNjgyNjIxODk0fQ.PDdXetHKNYTOyKP2Z7El86AKnMIVqZNQy4v2LYec0d4"; // aquí deberías obtener el token de autenticación desde el estado de Redux
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFlNGFhNTVkLTFiYjctNDMyMS1hMmVjLTQ4MDkzMDE1YzIzYiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4MTQyMTMwMiwiZXhwIjoxNjgyNjU0ODUwfQ.4D1rJKlx6guDSUyGRLTpZ6gLTERpbsIGgGPd65k0VjA"; // aquí deberías obtener el token de autenticación desde el estado de Redux
     const headers = getHeadersWithAuth(token);
     try {
       dispatch({ type: onLoading });
@@ -52,6 +70,24 @@ export const createturne = (payload, barber) => {
     }
   };
 };
+export const historyTurner = (payload) =>{
+  const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFlNGFhNTVkLTFiYjctNDMyMS1hMmVjLTQ4MDkzMDE1YzIzYiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4MTQyMTMwMiwiZXhwIjoxNjgyNjU0ODUwfQ.4D1rJKlx6guDSUyGRLTpZ6gLTERpbsIGgGPd65k0VjA"; // aquí deberías obtener el token de autenticación desde el estado de Redux
+const headers = getHeadersWithAuth(token);
+  return async (dispatch) => {
+    try {
+      dispatch({ type: onLoadingTurner})
+      const { data } = await axios.get(`http://localhost:8080/api/v1/appointments/${payload}`, {headers})
+      console.log(data)
+      if(data){
+        dispatch({type: onTurnerHistory, payload: data})
+      }
+    } catch (error) {
+      console.log(error, "error")
+      dispatch({type: onMessageTurnersError, error: error})
+    }
+  }
+}
 
-export const { onLoading, onTurner, onTurnerError } = turnerSlice.actions;
+export const { onLoading, onTurner, onTurnerError, onLoadingTurner, onMessageTurnersError, onTurnerHistory } = turnerSlice.actions;
 export default turnerSlice.reducer;
