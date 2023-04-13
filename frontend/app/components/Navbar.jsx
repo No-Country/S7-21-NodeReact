8073699349fc705d06a482ef3f696557fdff404c
fredyvaron <fvaron@itfip.edu.co>
2023-04-13 13:06:58 -0500

    solution bug button logout and turno

--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -9,95 +9,101 @@ import { useSelector } from "react-redux";
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const isAuthenticated = useSelector((state)=> state.user.status === "authenticated");
-  const Authuser = useSelector((state)=> state.user.user)
-  console.log("authuser", Authuser)
+  const isAuthenticated = useSelector(
+    (state) => state.user.status === "authenticated"
+  );
+  const Authuser = useSelector((state) => state.user.user);
+  console.log("authuser", Authuser);
   console.log("isAuthenticated", isAuthenticated);
   const toggle = () => {
     console.log("click en togle", isOpen);
     setIsOpen(!isOpen);
   };
   const togglemodal = () => {
-    setIsOpenModal(!isOpenModal)
-  }
+    setIsOpenModal(!isOpenModal);
+  };
 
   const closeModal = () => {
     setIsOpenModal(false);
   };
   return (
     <div className="content-nav-and-head">
- <header>
-      <div className="header">
-        <div className="left">
-          <TbClockHour9 />
-          Abierto: Lunes - Sabado, 09 am - 7:30 pm
-        </div>
-        <div className="right">
-          <div>
-            <FaWhatsapp />
-            Whatsapp
+      <header>
+        <div className="header">
+          <div className="left">
+            <TbClockHour9 />
+            Abierto: Lunes - Sabado, 09 am - 7:30 pm
           </div>
-          <div>
-            <HiOutlineMail />
-            thebossbarbershopco@gmail.com
+          <div className="right">
+            <div>
+              <FaWhatsapp />
+              Whatsapp
+            </div>
+            <div>
+              <HiOutlineMail />
+              thebossbarbershopco@gmail.com
+            </div>
           </div>
         </div>
-      </div>
-      <nav className="navbar">
-        <div className="logo">LOGO</div>
+        <nav className="navbar">
+          <div className="logo">LOGO</div>
 
-        <div className="hamburger" onClick={toggle}>
-          <div />
-          <div />
-          <div />
-        </div>
+          <div className="hamburger" onClick={toggle}>
+            <div />
+            <div />
+            <div />
+          </div>
 
-        <ul className={`nav-links ${isOpen ? "show-nav" : "navbar-hidden"}`}>
-          <li className="nav-item">
-            <Link to="/">INICIO</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/Barber/stylists">ESTILISTA/BARBERO</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/services">SERVICIOS</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/administrator">SHOP</Link>
-          </li>
-          <li>
-            <Link to="/about">SOBRE NOSOTROS</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact">CONTACTO</Link>
-          </li>
-          <li className="nav-item">
-            {isAuthenticated ? (<Link className="button" to="/">
-              LOGOUT
-            </Link>): (<Link className="button" to="/login">
-              INGRESAR
-            </Link>)}
-            
-          </li>
-          <li className="nav-item custom-style" onClick={togglemodal}>
-            <div className="buton">TURNO</div>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <Modal
+          <ul className={`nav-links ${isOpen ? "show-nav" : "navbar-hidden"}`}>
+            <li className="nav-item">
+              <Link to="/">INICIO</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Barber/stylists">ESTILISTA/BARBERO</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services">SERVICIOS</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/administrator">SHOP</Link>
+            </li>
+            <li>
+              <Link to="/about">SOBRE NOSOTROS</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact">CONTACTO</Link>
+            </li>
+            {isAuthenticated ? (
+              <li className="nav-item">
+                <div className="button-container">
+                  <Link className="button" to="/">
+                    LOGOUT
+                  </Link>
+                </div>
+                <div className="button-container" onClick={toggle}>
+                <div className="buton">TURNO</div>
+                </div>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <Link className="button" to="/login">
+                  INGRESAR
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </header>
+      <Modal
         isOpen={isOpenModal}
         onRequestClose={closeModal}
         contentLabel="Turno"
         className="modal"
-        
       >
         <div className="modal-content">
-        <Turno closeModal={closeModal}/>
+          <Turno closeModal={closeModal} />
         </div>
-       
       </Modal>
     </div>
-   
   );
 }
