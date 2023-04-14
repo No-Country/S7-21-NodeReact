fde596bab49924f8bfee5455e1436a61d1f25f77
Alberto <agomezjuan@hotmail.com>
2023-04-14 23:56:36 +0200

    feat(frontend): :sparkles: register is working well

--- frontend/app/components/RegisterForm.jsx
+++ frontend/app/components/RegisterForm.jsx
@@ -1,27 +1,39 @@
 import { Link, useNavigate } from "@remix-run/react";
-import { FaGoogle, FaFacebook } from "react-icons/fa";
+// import { FaGoogle, FaFacebook } from "react-icons/fa";
 import hairstyle from "~/assets/images/hairstyle.png";
 import { useDispatch } from "react-redux";
 import { registeruser } from "../store/auth/authSlice";
 import { useState } from "react";
+import ShowToast from "./ShowToast";
+
 export default function LoginForm() {
   const [acceptTerms, setAcceptTerms] = useState(false);
-  console.log(acceptTerms);
+  const [samePassword, setSamePassword] = useState("");
   const [user, setUser] = useState({
-    nombre: "",
-    apellido: "",
+    firstName: "",
+    lastName: "",
     email: "",
+    phone: "",
     password: "",
   });
+
+  const styles = {
+    border: "3px solid red",
+    backgroundColor: "pink",
+  };
+
   const dispacth = useDispatch();
   const navigate = useNavigate();
+
+
   const handlechange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    if (name !== "check-password") {
+    if (name == "check-password") {
+      setSamePassword(value);
+    } else {
       setUser({ ...user, [name]: value });
     }
-
   };
 
   const checkboxChange = (e) => {
@@ -29,10 +41,25 @@ export default function LoginForm() {
     setAcceptTerms(checked);
   };
 
-  const handleRegister = (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
-    console.log(user, "datos");
-    dispacth(registeruser(user))
+
+    if (!acceptTerms) {
+      alert("Debes aceptar los términos y condiciones");
+      return;
+    }
+    if (user.password !== samePassword) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+    let response = await dispacth(registeruser(user))
+
+    let status = response?.code === 201 ? true : false
+    ShowToast(status, status ? response?.body : response?.message, () => { });
+
+    response.code === 201
+      ? navigate("/login")
+      : console.log(response, "response");
   };
 
   return (
@@ -48,22 +75,22 @@ export default function LoginForm() {
             <div className="login-image">
               <img src={hairstyle} alt="Login" />
             </div>
-            <label htmlFor="nombre">Nombre</label>
+            <label htmlFor="firstName">Nombre</label>
             <input
               type="text"
-              id="nombre"
-              name="nombre"
+              id="firstName"
+              name="firstName"
               onChange={handlechange}
-              value={user.nombre}
+              value={user.firstName}
               required
             />
-            <label htmlFor="nombre">Apellido</label>
+            <label htmlFor="lastName">Apellido</label>
             <input
               type="text"
-              id="apellido"
-              name="apellido"
+              id="lastName"
+              name="lastName"
               onChange={handlechange}
-              value={user.apellido}
+              value={user.lastName}
               required
             />
 
@@ -77,38 +104,49 @@ export default function LoginForm() {
               required
             />
 
+            <label htmlFor="phone">Teléfono</label>
+            <input
+              type="phone"
+              id="phone"
+              name="phone"
+              onChange={handlechange}
+              value={user.phone}
+              required
+            />
+
             <label htmlFor="password">Contraseña</label>
             <input
               type="password"
               id="password"
               name="password"
               onChange={handlechange}
-              value={user.clave}
+              value={user.password}
               required
             />
 
             <label htmlFor="check-password">Repetir contraseña</label>
             <input
               type="password"
+              style={samePassword !== user.password ? styles : {}}
               id="check-password"
               name="check-password"
               onChange={handlechange}
-              value={user.clave}
+              value={samePassword}
               required
             />
 
             <div className="terms">
-              <input type="checkbox" id="acceptTerms" name="acceptTerms" checked={acceptTerms} onChange={checkboxChange} />
+              <input type="checkbox" id="acceptTerms" name="acceptTerms" checked={acceptTerms} onChange={checkboxChange} required />
               <label htmlFor="acceptTerms">&nbsp;Acepto los&nbsp;</label> <Link to="#" >terminos y condiciones</Link>
             </div>
 
-            <input type="submit" value={'Registrar'} />
+            <button type="submit">Registrar</button>
           </form>
           <p>
             ¿Ya tienes cuenta? <Link to="/login">Inicia Sesión</Link>
           </p>
 
-          <div className="login-social">
+          {/* <div className="login-social">
             <p>O inicia sesión con</p>
             <div className="login-social-icons">
               <Link to="/login/facebook">
@@ -118,7 +156,7 @@ export default function LoginForm() {
                 <FaGoogle />
               </Link>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
