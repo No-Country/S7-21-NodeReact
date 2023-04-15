b3fd4ba45b1a4544ef932d21d5e77ef036e96fcb
julandrod <julian.andres.rodriguez@gmail.com>
2023-04-15 14:47:34 -0500

    (backend) <fix> rutas para mostrar todos los servicios y un solo servicio ahora no piden rol ni token

--- backend/routes/servicesbarber.routes.js
+++ backend/routes/servicesbarber.routes.js
@@ -8,21 +8,17 @@ router.post(
   [authenticateUser, authorizeByRole("admin")],
   sBarberCtrls.postServiceBarber
 );
-router.get(
-  "/",
-  [authenticateUser, authorizeByRole("admin")],
-  sBarberCtrls.getServicesBarber
-);
-router.get(
-  "/:serviceId",
-  [authenticateUser, authorizeByRole("admin")],
-  sBarberCtrls.getSingleServiceBarber
-);
+
+router.get("/", sBarberCtrls.getServicesBarber);
+
+router.get("/:serviceId", sBarberCtrls.getSingleServiceBarber);
+
 router.patch(
   "/:serviceId",
   [authenticateUser, authorizeByRole("admin")],
   sBarberCtrls.patchServiceBarber
 );
+
 router.delete(
   "/:serviceId",
   [authenticateUser, authorizeByRole("admin")],
